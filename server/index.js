f2bda5e0bba0c9e59c4b786d1090e6ee12b7903e
Guillermo Galarza <Galarzaguillermo.ggm@gmail.com>
2023-07-08 11:23:59 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, async () => {
-    console.log("🙌 Listening at 3001"); 
+  server.listen(() => {
+    console.log("🙌 Server listening"); 
   });
 });
