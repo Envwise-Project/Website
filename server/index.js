82f195228462d1027356d4579c162804dc237b1b
Guillermo Galarza <Galarzaguillermo.ggm@gmail.com>
2023-07-10 06:52:46 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const port = process.env.PORT || 3000;
+const port = process.env.AZURE_POSTGRESQL_PORT || 3000;
 
 conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
