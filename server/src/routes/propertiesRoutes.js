e139c0d1fc874f4008bd4197aa330e8688a083f9
madahva <galarzaguillermo.ggm@gmail.com>
2023-07-18 18:19:08 -0300

    Feat (Server): Get Filtered properties

--- server/src/routes/propertiesRoutes.js
+++ server/src/routes/propertiesRoutes.js
@@ -1,12 +1,17 @@
 const { Router } = require("express");
-const { createPropertyHandler } = require("../handlers/createPropertyHandler.js");
+const {
+  createPropertyHandler,
+} = require("../handlers/createPropertyHandler.js");
 const {
   getAllPropertiesHandler,
 } = require("../handlers/getAllPropertiesHandler.js");
+const {
+  getFilteredPropertiesHandler,
+} = require("../handlers/getFilteredPropertiesHandler.js");
 const propertyRouter = Router();
 
 propertyRouter.get("/", getAllPropertiesHandler);
 propertyRouter.post("/", createPropertyHandler);
-
+propertyRouter.get("/filter", getFilteredPropertiesHandler);
 
 module.exports = propertyRouter;
